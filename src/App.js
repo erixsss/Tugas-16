f7a7bf600e99fd608666c9c448c266bb6c229454
Erik Gustiar <43042490+erixsss@users.noreply.github.com>
2018-10-27 21:25:22 +0700

    counter component nya terpisah sendiri

--- src/App.js
+++ src/App.js
@@ -1,43 +1,13 @@
-import React, { Component } from "react";
-
-class App extends Component {
-  state = { value: 0 };
-
-  tambah() {
-    this.setState({
-      value: this.state.value + 1
-    });
-    this.log();
-  }
-
-  kurang() {
-    this.setState({
-      value: this.state.value - 1
-    });
-    this.log();
-  }
-
-  reset() {
-    this.setState({
-      value: 0
-    });
-  }
-
-  log() {
-    console.clear();
-    console.log("tombol ditekan = " + this.state.value);
-  }
-
+import React from "react";
+import Counter from "./Component/Counter";
+class App extends React.Component {
   render() {
-    console.log("setelah render = " + this.state.value);
     return (
       <div>
         <h1>COUNTER</h1>
-        <h2>{this.state.value}</h2>
-        <button onClick={() => this.tambah()}>Tambah</button>
-        <button onClick={() => this.kurang()}>Kurang</button>
-        <br />
-        <button onClick={() => this.reset()}>reset</button>
+        <Counter item="makan" />
+        <Counter item="minum" />
+        <Counter item="lari" />
       </div>
     );
   }
---
+++ src/Component/Counter.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = { value: 0 };
+
+  tambah() {
+    this.setState({
+      value: this.state.value + 1
+    });
+    this.log();
+  }
+
+  kurang() {
+    this.setState({
+      value: this.state.value - 1
+    });
+    this.log();
+  }
+
+  reset() {
+    this.setState({
+      value: 0
+    });
+  }
+
+  log() {
+    console.clear();
+    console.log("tombol ditekan = " + this.state.value);
+  }
+  render() {
+    console.log("setelah render = " + this.state.value);
+    return (
+      <div>
+        <h2>{this.props.item}</h2>
+        <h2>{this.state.value}</h2>
+        <button onClick={() => this.tambah()}>Tambah</button>
+        <button onClick={() => this.kurang()}>Kurang</button>
+        <br />
+        <button onClick={() => this.reset()}>reset</button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
